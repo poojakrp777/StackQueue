fbe40240afff59aea4d63349c2b2985693f65c2b
poojakrp777 <poojakrp777@gmail.com>
2023-02-14 21:48:48 +0530

    [POOJA] Ability to create a stack using linkedlist

--- StackQueue/Program.cs
+++ StackQueue/Program.cs
@@ -11,6 +11,11 @@ namespace StackQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Stacks and Queues Programs");
+            StackUsingLinkList stackUsingLinkList = new StackUsingLinkList();
+            stackUsingLinkList.push(70);
+            stackUsingLinkList.push(30);
+            stackUsingLinkList.push(56);
+            stackUsingLinkList.Display();
         }
     }
 }
